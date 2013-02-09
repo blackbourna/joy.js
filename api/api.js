70a9034cfe8786aacabf6cd3efbb2bd923c6eaa4
Endel Dreyer <endel.dreyer@gmail.com>
2013-02-09 20:49:28 -0200

    Update documentation.

---
+++ api/api.js
@@ -0,0 +1,51 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Button",
+        "Circle",
+        "Color",
+        "CompositeOperation",
+        "Context2d",
+        "DisplayObject",
+        "DisplayObjectContainer",
+        "Engine",
+        "Keyboard",
+        "Math",
+        "Matrix2D",
+        "Mouse",
+        "Movimentation",
+        "Parallax",
+        "Rect",
+        "RectCollider",
+        "Scene",
+        "Shader",
+        "Sound",
+        "Sprite",
+        "SpriteSheet",
+        "Support",
+        "Text",
+        "Tilemap",
+        "Tileset",
+        "Triggerable",
+        "Tween",
+        "TweenManager",
+        "Utils",
+        "Vector2d",
+        "Viewport"
+    ],
+    "modules": [
+        "Joy",
+        "Joy.Behaviour"
+    ],
+    "allModules": [
+        {
+            "displayName": "Joy",
+            "name": "Joy"
+        },
+        {
+            "displayName": "Joy.Behaviour",
+            "name": "Joy.Behaviour"
+        }
+    ]
+} };
+});
